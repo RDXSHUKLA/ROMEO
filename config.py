7deff0d9d27382d9fefa97c742c47b375c3fa4ef
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2025-01-27 20:13:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/XFLSWAN/LUDO-MUSIC",
+    "https://github.com/RDXSHUKLA/ROMEO",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
